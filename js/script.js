e1fe951db3090a22791bdb093dcf83da27002a62
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2024-09-23 17:54:44 -0300

    feat: adicionando a function calcular com condições lógicas e um switch

--- js/script.js
+++ js/script.js
@@ -29,3 +29,31 @@ function apagar() {
         document.getElementById('display').value = valorAtual;
     }
 }
+
+function calcular() {
+    if (!valorAnterior || !operador || !valorAtual) return;
+
+    const num1 = parseFloat(valorAnterior);
+    const num2 = parseFloat(valorAtual);
+    let resultado = 0;
+
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            resultado = num2 !== 0 ? num1 / num2 : 'Erro';
+            break;
+    }
+
+    document.getElementById('display').value = resultado;
+    valorAnterior = resultado.toString();
+    valorAtual = '';
+    operador = '';
+}
